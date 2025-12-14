7611a7429123c87dc97d60da3f698e13aec74e98
Kevin Watke <kevinengineers@proton.me>
2025-12-13 18:54:42 -0700

    added getCurrentTodos() and printCurrentTodos(), setup some structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? todoInput = null;
+string[] todoArray = new string[100];
+void promptForTodoGreeting()
+{
+  Console.WriteLine("Hello, this is a Todo app, written in C#");
+  Console.Write("Enter your first Todo: ");
+  todoInput = Console.ReadLine();
+}
+
+String[] getCurrentTodos()
+{
+  return todoArray; 
+}
+
+void printCurrentTodos()
+{
+  //FIXME: This is wrong. The array is initalized with 100 null items, fix this so if the item is null
+  // it doesn't print the item out.
+  if (todoArray.Length > 0)
+  {
+    foreach (string todo in todoArray)
+    {
+       Console.WriteLine($"Todo: {todo}");
+    }
+  }
+}
+
+promptForTodoGreeting();
+printCurrentTodos();
+
+
